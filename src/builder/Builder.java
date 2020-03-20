f85bc826caa6e41fac5bab395448f28e224bf6ce
thomas <thomasholleman@gmail.com>
2020-03-20 11:59:35 +0100

    More readable code

--- src/builder/Builder.java
+++ src/builder/Builder.java
@@ -1,18 +1,20 @@
 package builder;
 
+import builder.MultiThread.MinifyThread;
+import builder.MultiThread.PackDotMCMetaCreator;
+import builder.MultiThread.ParseThread;
 import builder.constants.ErrorMessages;
-import builder.json.Minify;
-import builder.parser.Parser;
-import builder.parser.ParsingException;
 import builder.properties.Properties;
 import builder.zipper.Zipper;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Vector;
+import java.nio.file.Path;
 
-import static builder.constants.ErrorMessages.COULD_NOT_CREATE_PACK_MCMETA;
+import static builder.MultiThread.Copy;
+import static builder.MultiThread.rejoin;
 import static builder.constants.FileStrings.*;
 import static builder.properties.Properties.*;
 
@@ -23,6 +25,8 @@ import static builder.properties.Properties.*;
  */
 public class Builder
 {
+	private Builder() {}
+	
 	/**
 	 * Build the datapack using the files from {@link builder.constants.FileStrings#SOURCE_DIRECTORY}
 	 * into {@link builder.constants.FileStrings#OUTPUT_DIRECTORY} and into a {@link builder.constants.FileStrings#ZIP} file.
@@ -43,21 +47,7 @@ public class Builder
 		new PackDotMCMetaCreator(Properties.DESCRIPTION).start();
 		File[] toZip = getFilesToZip();
 		
-		for (Thread thread : MultiThread.threads)
-		{
-			thread.join();
-		}
-		
-		// All threads are now back into this one
-		
-		if (!MultiThread.failures.isEmpty())
-		{
-			for (Throwable failure : MultiThread.failures)
-			{
-				failure.printStackTrace();
-			}
-			return;
-		}
+		rejoin();
 		
 		System.out.println("All files inside \"" + SOURCE_DIRECTORY + "\" are now parsed and ready to be used");
 		
@@ -71,8 +61,6 @@ public class Builder
 		System.out.println("The datapack is now a .zip file and ready to be distributed");
 	}
 	
-	private Builder() {}
-	
 	private static String getDestZipFile()
 	{
 		return DATAPACK_NAME + " " + CURRENT_MINECRAFT_VERSION + COMPILE_LEVEL.zipSuffix + ZIP;
@@ -83,152 +71,70 @@ public class Builder
 		return new File[]{new File(PACK_DOT_MCMETA), new File(OUTPUT_DIRECTORY)};
 	}
 	
-	private static void iterate(File f) throws IOException
+	private static boolean iterate(File f) throws IOException
 	{
 		assert f != null;
 		if (f.isDirectory())
 		{
-			File dataVersion = new File(dataVersion(f.getPath()));
-			if (!dataVersion.exists() && !dataVersion.mkdir())
-			{
-				throw new BuildException(ErrorMessages.COULD_NOT_CREATE_DIRECTORY);
-			}
-			File[] files = f.listFiles();
-			assert files != null;
-			for (File file : files)
-			{
-				iterate(file);
-			}
-			return;
+			return handleDirectory(f);
 		}
+		return handleFile(f);
+	}
+	
+	private static boolean handleDirectory(File f) throws IOException
+	{
+		File dataVersion = new File(dataVersion(f));
+		if (!dataVersion.exists() && !dataVersion.mkdir())
+		{
+			throw new BuildException(ErrorMessages.COULD_NOT_CREATE_DIRECTORY);
+		}
+		
+		boolean filled = false;
+		
+		File[] files = f.listFiles();
+		assert files != null;
+		for (File file : files)
+		{
+			filled = iterate(file) || filled;
+		}
+		
+		if (!filled)
+		{
+			Files.delete(dataVersion.toPath());
+		}
+		return filled;
+	}
+	
+	private static boolean handleFile(File f) throws FileNotFoundException
+	{
+		assert f.isFile();
 		
 		for (String s : zipBlackList())
 		{
-			if (f.getName().matches(s)) return;
+			if (f.getName().matches(s)) return false;
 		}
 		
-		if (parseFile(f, dataVersion(f.getPath()))) return;
-		
-		Files.copy(f.toPath(), new File(dataVersion(f.getPath())).toPath(), StandardCopyOption.REPLACE_EXISTING);
-	}
-	
-	private static boolean parseFile(File f, String output) throws FileNotFoundException
-	{
 		for (String s : parseWhiteList())
 		{
 			if (f.getName().matches(s))
 			{
-				new ParseThread(f, output, COMPILE_LEVEL.level).start();
+				new ParseThread(f, dataVersion(f), COMPILE_LEVEL.level).start();
 				return true;
 			}
 		}
 		
 		if (f.getName().endsWith(JSON))
 		{
-			new MinifyThread(f, output).start();
+			new MinifyThread(f, dataVersion(f)).start();
 			return true;
 		}
-		return false;
+		
+		new Copy(f.toPath(), Path.of(dataVersion(f))).start();
+		return true;
 	}
 	
-	private abstract static class MultiThread extends Thread
+	private static String dataVersion(File file)
 	{
-		private static final Vector<Thread> threads = new Vector<>();
-		private static final Vector<Throwable> failures = new Vector<>();
-		
-		public MultiThread()
-		{
-			threads.add(this);
-			setUncaughtExceptionHandler((t, e) -> failures.add(e));
-		}
-		
-		@Override
-		public abstract void run();
-	}
-	
-	private static class ParseThread extends MultiThread
-	{
-		public final File f;
-		public final String output;
-		public final int compileLevel;
-		
-		private ParseThread(File f, String output, int compileLevel)
-		{
-			this.f = f;
-			this.output = output;
-			this.compileLevel = compileLevel;
-		}
-		
-		@Override
-		public void run()
-		{
-			try
-			{
-				Parser.parse(f, output, compileLevel);
-			}
-			catch (ParsingException pEx)
-			{
-				throw new ParsingException(ErrorMessages.AN_ERROR_OCCURRED_WHILE_PARSING(f.getName()), pEx);
-			}
-		}
-	}
-	
-	private static class MinifyThread extends MultiThread
-	{
-		private final FileInputStream inputStream;
-		private final FileOutputStream outputStream;
-		
-		private MinifyThread(File input, String output) throws FileNotFoundException
-		{
-			inputStream = new FileInputStream(input);
-			outputStream = new FileOutputStream(new File(output));
-		}
-		
-		@Override
-		public void run()
-		{
-			try
-			{
-				new Minify().minify(inputStream, outputStream);
-			}
-			catch (Exception e)
-			{
-				throw new BuildException(e.getMessage(), e);
-			}
-		}
-	}
-	
-	private static class PackDotMCMetaCreator extends MultiThread
-	{
-		private final String description;
-		
-		public PackDotMCMetaCreator(String description)
-		{
-			this.description = description;
-		}
-		
-		@Override
-		public void run()
-		{
-			try (FileOutputStream fileOutputStream = new FileOutputStream(new File(PACK_DOT_MCMETA)))
-			{
-				fileOutputStream.write(("{" +
-				                        "\"pack\":{" +
-				                        "\"pack_format\":5," +
-				                        "\"description\":\"" + description + "\"" +
-				                        "}" +
-				                        "}").getBytes());
-				fileOutputStream.flush();
-			}
-			catch (IOException e)
-			{
-				throw new BuildException(COULD_NOT_CREATE_PACK_MCMETA, e);
-			}
-		}
-	}
-	
-	private static String dataVersion(String path)
-	{
-		return path.replaceFirst(SOURCE_DIRECTORY, OUTPUT_DIRECTORY);
+		return file.getPath().replaceFirst(SOURCE_DIRECTORY, OUTPUT_DIRECTORY);
 	}
 }
---
+++ src/builder/MultiThread.java
@@ -0,0 +1,152 @@
+package builder;
+
+import builder.constants.ErrorMessages;
+import builder.json.Minify;
+import builder.parser.Parser;
+import builder.parser.ParsingException;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Vector;
+
+import static builder.constants.ErrorMessages.COULD_NOT_CREATE_PACK_MCMETA;
+import static builder.constants.FileStrings.PACK_DOT_MCMETA;
+
+public abstract class MultiThread extends Thread
+{
+	private static final Vector<Thread> threads = new Vector<>();
+	private static final Vector<Throwable> failures = new Vector<>();
+	
+	protected MultiThread()
+	{
+		threads.add(this);
+		setUncaughtExceptionHandler((t, e) -> failures.add(e));
+	}
+	
+	@Override
+	public abstract void run();
+	
+	public static void rejoin() throws InterruptedException
+	{
+		for (Thread thread : threads)
+		{
+			thread.join();
+		}
+		if (!failures.isEmpty())
+		{
+			for (Throwable failure : failures)
+			{
+				failure.printStackTrace();
+			}
+			throw new BuildException(failures.size() + " exception" + (failures.size() == 1 ? "" : "s") + ". " +
+			                         "First exception: ", failures.get(0));
+		}
+	}
+	
+	public static class ParseThread extends MultiThread
+	{
+		public final File f;
+		public final String output;
+		public final int compileLevel;
+		
+		public ParseThread(File f, String output, int compileLevel)
+		{
+			this.f = f;
+			this.output = output;
+			this.compileLevel = compileLevel;
+		}
+		
+		@Override
+		public void run()
+		{
+			try
+			{
+				Parser.parse(f, output, compileLevel);
+			}
+			catch (ParsingException pEx)
+			{
+				throw new ParsingException(ErrorMessages.AN_ERROR_OCCURRED_WHILE_PARSING(f.getName()), pEx);
+			}
+		}
+	}
+	
+	public static class MinifyThread extends MultiThread
+	{
+		private final FileInputStream inputStream;
+		private final FileOutputStream outputStream;
+		
+		public MinifyThread(File input, String output) throws FileNotFoundException
+		{
+			inputStream = new FileInputStream(input);
+			outputStream = new FileOutputStream(new File(output));
+		}
+		
+		@Override
+		public void run()
+		{
+			try
+			{
+				new Minify().minify(inputStream, outputStream);
+			}
+			catch (Exception e)
+			{
+				throw new BuildException(e.getMessage(), e);
+			}
+		}
+	}
+	
+	public static class PackDotMCMetaCreator extends MultiThread
+	{
+		private final String description;
+		
+		public PackDotMCMetaCreator(String description)
+		{
+			this.description = description;
+		}
+		
+		@Override
+		public void run()
+		{
+			try (FileOutputStream fileOutputStream = new FileOutputStream(new File(PACK_DOT_MCMETA)))
+			{
+				fileOutputStream.write(("{" +
+				                        "\"pack\":{" +
+				                        "\"pack_format\":5," +
+				                        "\"description\":\"" + description + "\"" +
+				                        "}" +
+				                        "}").getBytes());
+				fileOutputStream.flush();
+			}
+			catch (IOException e)
+			{
+				throw new BuildException(COULD_NOT_CREATE_PACK_MCMETA, e);
+			}
+		}
+	}
+	
+	public static class Copy extends MultiThread
+	{
+		private final Path from;
+		private final Path to;
+		
+		public Copy(Path from, Path to) {
+			this.from = from;
+			this.to = to;
+		}
+		
+		@Override
+		public void run()
+		{
+			try
+			{
+				Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+			}
+			catch (IOException e)
+			{
+				throw new BuildException("Could not copy", e);
+			}
+		}
+	}
+}
